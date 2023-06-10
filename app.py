5fcaaa5d0612959024d39fd837cc8cf1b881eb50
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-06-10 13:52:16 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    fopen=open("test.txt","w")
+    fopen.write("Hello World!")
+    fopen.close()
     return render_template("index.html", title="Hello")
